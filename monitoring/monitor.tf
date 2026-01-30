e7c8c45be963e1839005c99877667564ad8aa72c
AdewaleOpeloyeru <Opeloyeruadewale@icloud.com>
2026-01-30 21:22:37 +0000

    add checkov scan

--- monitoring/monitor.tf
+++ monitoring/monitor.tf
@@ -2,9 +2,7 @@ data "ibm_resource_group" "group" {
   name = "Default"
 }
 
-module "monitoring" {
-  # source  = "terraform-ibm-modules/cloud-monitoring/ibm"
-  # version = "1.9.0" 
+module "monitoring" { 
   source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-cloud-monitoring.git?ref=d147145b08e6ddadf2756cfb096a4174d580a4b8"
 
   resource_group_id = data.ibm_resource_group.group.id

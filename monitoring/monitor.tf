ca5c3ee285eaf99c674513099eabe81969e9234c
AdewaleOpeloyeru <Opeloyeruadewale@icloud.com>
2026-01-30 20:48:12 +0000

    add checkov scan

--- monitoring/monitor.tf
+++ monitoring/monitor.tf
@@ -5,7 +5,7 @@ data "ibm_resource_group" "group" {
 module "monitoring" {
   # source  = "terraform-ibm-modules/cloud-monitoring/ibm"
   # version = "1.9.0" 
-  source = git::https://github.com/terraform-ibm-modules/terraform-ibm-cloud-monitoring.git?ref=d147145b08e6ddadf2756cfb096a4174d580a4b8
+  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-cloud-monitoring.git?ref=d147145b08e6ddadf2756cfb096a4174d580a4b8"
 
   resource_group_id = data.ibm_resource_group.group.id
   region            = "eu-gb" # you can change this to your own region
